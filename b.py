590ab543861524fb3e02a8ac3c26cf4fd43c97f2
Aravind Reddy <aravindreddy255@gmail.com>
2016-05-10 20:30:22 +0530

    added play grid design

---
+++ b.py
@@ -0,0 +1,4 @@
+
+def func():
+	for i in range(35):
+		print 'xx',
---
+++ create.py
@@ -0,0 +1,25 @@
+"""
+	xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+	xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+	xx...................................................xx
+	xx...xx.........................................xx...xx
+	xx....xx.......................................xx....xx
+	xx.....xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.....xx
+	xx...................................................xx
+	xx...................................................xx
+	xx...................................................xx
+	xx........xxxxxxx.......xxxxxxx.......xxxxxxx........xx
+	xx........x.....x.......x.....x.......x..............xx
+	xx........x.....x.......x.....x.......x..............xx
+	xx........x..xxxx.......x.xxx.x.......x..............xx
+	xx........x.............x.....x.......x..............xx
+	xx........x.............x.....x.......x..............xx
+	xx........x.............x.....x.......xxxxxxx........xx
+	xx...................................................xx
+	xx...................................................xx
+	xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+	xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+	20x55 play grid
+"""
+
