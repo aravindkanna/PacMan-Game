adbbb1c90b1bb08bf1053420320758dd8a532bb6
Aravind Reddy <aravindreddy255@gmail.com>
2016-05-12 08:40:58 +0530

    completed pacman part

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 from create import *
 from person import *
 from ghost import *
+from pacman import *
 
 grid = create_grid()
 print_grid(grid)
@@ -8,6 +9,8 @@ print_grid(grid)
 g1 = Ghost(grid, 3, 4)
 g2 = Ghost(grid, 17, 4)
 #print_grid(grid)
+
+p = Pacman(grid, 4, 20)
 
 score = 0
 while 1:
@@ -17,9 +20,17 @@ while 1:
 	print score
 
 	print "Move: ",
-	move = raw_input()
-	print move
+	mov = raw_input()
+	print mov
 
-	if move == 'q':
+	if mov == 'q':
 		print "Exit..!!"
 		break
+
+	elif mov=='w' or mov=='s' or mov=='a' or mov=='d' : 
+		p.move(grid, mov)
+
+	else:
+		print "Please enter correct keys..!!!"
+
+	
--- pacman.py
+++ pacman.py
@@ -1,2 +1,11 @@
 from person import *
 
+class Pacman(Person):
+
+	def __init__(self, grid, x, y):
+		self.x = x
+		self.y = y
+		grid[x][y] = 'P'
+
+	def set_pos(self, grid):
+		grid[self.x][self.y] = 'P'
